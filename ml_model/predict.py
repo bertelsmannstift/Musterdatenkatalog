9c95e2d75658b5e89223dd24c983356205e8c55d
Mila Frerichs <mila.frerichs@gmail.com>
2021-05-31 22:13:21 +0200

    feat: add prediction of new datasets
    add the prediction of new data based on a previously trained and saved
    model for predicting musterdatensätze

---
+++ ml_model/predict.py
@@ -0,0 +1,56 @@
+import pickle
+from datetime import datetime
+import pandas as pd
+
+TITLE_COLUMNS = ['dct:title']
+THEME_COLUMNS = ['dcat:theme']
+DESCRIPTION_COLUMNS = ['dct:description']
+
+COLUMNS = TITLE_COLUMNS + THEME_COLUMNS + DESCRIPTION_COLUMNS
+
+def load_model(model_file):
+    infile = open(model_file, "rb")
+    model = pickle.load(infile)
+    infile.close()
+    return model
+
+def prepare_data(raw_data):
+    data = raw_data[COLUMNS]
+    data.fillna("", inplace=True)
+    return data
+
+
+def probability(filename, model_file):
+    raw_data = pd.read_csv(filename, dtype=str)
+    model = load_model(model_file)
+    data = prepare_data(raw_data)
+    proba = model.predict_proba(data)
+
+    print(proba)
+    #save probabilities
+    probablilty_result_filename = f"mdk_predicted_probability_result_{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
+    outfile = open(probablilty_result_filename, "wb")
+    pickle.dump(proba, outfile)
+    outfile.close()
+
+
+def predict(filename, model_file):
+    raw_data = pd.read_csv(filename, dtype=str)
+    raw_data["dct:description_bkp"] = raw_data['dct:description'].map(str)
+    raw_data["dct:description"] = raw_data['dct:description'].map(str) + ',' + raw_data['tags'].map(str)
+    model = load_model(model_file)
+    data = prepare_data(raw_data)
+    result = model.predict(data)
+    raw_data["MUSTERDATENSATZ"] = result
+
+    result_filename = f"results/mdk_predicted_result_{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
+    raw_data.to_csv(result_filename, index=False)
+
+def main():
+    model_filename = "models/model_86.020210219-113102.pickle"
+    predict("predicting/govdata_results_20210301-153233.csv", model_filename)
+
+if __name__ == "__main__":
+    main()
+
+
