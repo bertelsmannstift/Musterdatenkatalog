a95f26ce56e705242069888d360fb814637079e4
Mila Frerichs <mila.frerichs@gmail.com>
2022-02-04 14:25:19 +0100

    feat: add URI Definitionen Webseite

---
+++ mdk-definitions/assets/css/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './assets/css/tailwind.config.js'  },
+    autoprefixer: {},
+  },
+};
---
+++ mdk-definitions/content/def/_index.md
@@ -0,0 +1,4 @@
+---
+title: "Definitionen"
+---
+# Definitionen des Musterdatenkataloges
